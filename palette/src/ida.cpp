42d2d41d288598a8ebffc4971fcb965d02b86377
Jinmo <santoky001@naver.com>
2019-03-07 17:55:52 +0900

    rename & optimize

--- palette/src/ida.cpp
+++ palette/src/ida.cpp
@@ -14,131 +14,131 @@
 QHash<QString, QDate> g_last_used;
 
 struct Action {
-    QString id, tooltip, shortcut;
+	QString id, tooltip, shortcut;
 
-    Action(qstring id_, qstring tooltip_, qstring shortcut_)
-            : id(QString(id_.c_str())),
-              tooltip(QString(tooltip_.c_str())),
-              shortcut(QString(shortcut_.c_str())) {}
+	Action(qstring id_, qstring tooltip_, qstring shortcut_)
+		: id(QString(id_.c_str())),
+		tooltip(QString(tooltip_.c_str())),
+		shortcut(QString(shortcut_.c_str())) {}
 };
 
-std::vector<Action *> cached_actions;
+std::vector<Action*> cached_actions;
 
 QVector<QRegularExpression> getBlacklist() {
-    auto blacklist = config()["blacklist"].toArray();
-    QVector<QRegularExpression> blacklist_converted;
+	auto blacklist = config()["blacklist"].toArray();
+	QVector<QRegularExpression> blacklist_converted;
 
-    for (auto &i : blacklist) {
-        if (i.toString().size())
-            blacklist_converted.push_back(QRegularExpression(i.toString()));
-    }
-    return blacklist_converted;
+	for (auto& i : blacklist) {
+		if (i.toString().size())
+			blacklist_converted.push_back(QRegularExpression(i.toString()));
+	}
+	return blacklist_converted;
 }
 
-std::vector<Action *> *getActions() {
-    qstrvec_t id_list;
-    get_registered_actions(&id_list);
+std::vector<Action*>* getActions() {
+	qstrvec_t id_list;
+	get_registered_actions(&id_list);
 
-    size_t names_count = get_nlist_size();
+	size_t names_count = get_nlist_size();
 
-    if (cached_actions.size() == id_list.size() + names_count)
-        return &cached_actions;
+	if (cached_actions.size() == id_list.size() + names_count)
+		return &cached_actions;
 
-    auto *result = new std::vector<Action *>();
-    auto blacklist = getBlacklist();
+	auto * result = new std::vector<Action*>();
+	auto blacklist = getBlacklist();
 
-    result->reserve(id_list.size());
+	result->reserve(id_list.size());
 
-    // Variables used in the loop below
-    qstring tooltip, shortcut;
-    action_state_t state;
+	// Variables used in the loop below
+	qstring tooltip, shortcut;
+	action_state_t state;
 
-    for (auto &item : id_list) {
-        bool skip = false;
-        for (auto &pattern : blacklist)
-            if (pattern.match(item.c_str()).hasMatch()) {
-                skip = true;
-                break;
-            }
+	for (auto& item : id_list) {
+		bool skip = false;
+		for (auto& pattern : blacklist)
+			if (pattern.match(item.c_str()).hasMatch()) {
+				skip = true;
+				break;
+			}
 
-        if (skip)
-            continue;
+		if (skip)
+			continue;
 
-        if (!get_action_state(item.c_str(), &state))
-            continue;
+		if (!get_action_state(item.c_str(), &state))
+			continue;
 
-        if (state > AST_ENABLE)
-            continue;
+		if (state > AST_ENABLE)
+			continue;
 
-        get_action_tooltip(&tooltip, item.c_str());
-        get_action_shortcut(&shortcut, item.c_str());
-        result->push_back(new Action(item, tooltip, shortcut));
-    }
+		get_action_tooltip(&tooltip, item.c_str());
+		get_action_shortcut(&shortcut, item.c_str());
+		result->push_back(new Action(item, tooltip, shortcut));
+	}
 
-    for(size_t i = 0; i < names_count; i++) {
-        result->push_back(new Action(qstring(("@ " + QString::number(i, 16)).toStdString().c_str()), get_nlist_name(i), ""));
-    }
+	for (size_t i = 0; i < names_count; i++) {
+		result->push_back(new Action(qstring(("@ " + QString::number(i, 16)).toStdString().c_str()), get_nlist_name(i), ""));
+	}
 
-    cached_actions = *result;
+	cached_actions = *result;
 
-    return result;
+	return result;
 }
 
 class QIDACommandPaletteInner : public QPaletteInner {
 public:
-    QIDACommandPaletteInner(QWidget *parent, QObject *prevItem)
-            : QPaletteInner(parent, prevItem) {
-        populateList();
-    }
+	QIDACommandPaletteInner(QWidget* parent, QObject* prevItem)
+		: QPaletteInner(parent, prevItem) {
+		populateList();
+	}
 
-    EnterResult enter_callback() override {
-        processEnterResult(true);
+	EnterResult enter_callback() override {
+		processEnterResult(true);
 
-        auto *model = entries_->model();
-        auto id = model->data(model->index(entries_->currentIndex().row(), 2)).toString();
-        g_last_used[id] = QDate::currentDate();
-        if(id.startsWith("@ "))
-            jumpto(id.mid(2).toUInt(nullptr, 16));
-        process_ui_action(id.toStdString().c_str());
-        // already hidden
-        return false;
-    }
+		auto* model = entries_->model();
+		auto id = model->data(model->index(entries_->currentIndex().row(), 2)).toString();
+		g_last_used[id] = QDate::currentDate();
+		if (id.startsWith("@ "))
+			jumpto(id.mid(2).toUInt(nullptr, 16));
+		process_ui_action(id.toStdString().c_str());
+		// already hidden
+		return false;
+	}
 
-    void populateList() {
-        auto out = getActions();
-        auto *source = entries_->source();
+	void populateList() {
+		auto out = getActions();
+		auto* source = entries_->source();
 
-        source->setRowCount(static_cast<int>(out->size()));
-        source->setColumnCount(3);
+		source->setRowCount(static_cast<int>(out->size()));
+		source->setColumnCount(3);
 
-        int i = 0;
-        for (auto &item : *out) {
-            source->setData(source->index(i, 0), item->tooltip);
-            source->setData(source->index(i, 1), item->shortcut);
-            source->setData(source->index(i, 2), item->id);
-            i += 1;
-        }
-    }
+		int i = 0;
+		for (auto& item : *out) {
+			source->setData(source->index(i, 0), item->tooltip);
+			source->setData(source->index(i, 1), item->shortcut);
+			source->setData(source->index(i, 2), item->id);
+			i += 1;
+		}
+	}
 };
 
-QPaletteContainer *curWidget;
+QPaletteContainer* curWidget;
 
 class enter_handler : public action_handler_t {
-    int idaapi activate(action_activation_ctx_t *) override {
-        if (!curWidget)
-            curWidget = new QPaletteContainer();
+	int idaapi activate(action_activation_ctx_t*) override {
+		if (!curWidget)
+			curWidget = new QPaletteContainer();
 
-        curWidget->clear();
-        curWidget->add(new QIDACommandPaletteInner(curWidget, nullptr));
-        curWidget->show();
-        curWidget->showWidget(0);
+		curWidget->clear();
+		curWidget->add(new QIDACommandPaletteInner(curWidget, nullptr));
+		curWidget->show();
+		curWidget->showWidget(0);
 
-        return 1;
-    }
+		return 1;
+	}
 
-    action_state_t idaapi update(action_update_ctx_t *) override {
-        return AST_ENABLE_ALWAYS;
-    }
+	action_state_t idaapi update(action_update_ctx_t*) override {
+		return AST_ENABLE_ALWAYS;
+	}
 };
 
 extern plugin_t PLUGIN;
@@ -146,16 +146,16 @@ extern plugin_t PLUGIN;
 static enter_handler enter_handler_;
 
 static action_desc_t enter_action = ACTION_DESC_LITERAL(
-        "ifred:enter",
-        "ifred command palette",
-        &enter_handler_,
-        "Ctrl+Shift+P",
-        "command palette",
-        -1);
+	"ifred:enter",
+	"ifred command palette",
+	&enter_handler_,
+	"Ctrl+Shift+P",
+	"command palette",
+	-1);
 
 //--------------------------------------------------------------------------
 bool idaapi run(size_t) {
-    return true;
+	return true;
 }
 
 extern char comment[], help[], wanted_name[], wanted_hotkey[];
@@ -164,7 +164,7 @@ extern char comment[], help[], wanted_name[], wanted_hotkey[];
 char comment[] = "ifred";
 
 char help[] =
-        "IDA palette";
+"IDA palette";
 
 //--------------------------------------------------------------------------
 // This is the preferred name of the plugin module in the menu system
@@ -174,25 +174,26 @@ char wanted_name[] = "ifred";
 
 //--------------------------------------------------------------------------
 int idaapi init(void) {
-    // the plugin works only with idaq
-    int r = is_idaq() ? PLUGIN_KEEP : PLUGIN_SKIP;
-    if (r == PLUGIN_KEEP) {
-        msg("ifred loading...\n");
-        getActions();
+	// the plugin works only with idaq
+	int r = is_idaq() ? PLUGIN_KEEP : PLUGIN_SKIP;
+	if (r == PLUGIN_KEEP) {
+		msg("ifred loading...\n");
+		getActions();
 
-        if (!register_action(enter_action)) {
-            msg("ifred action loading error");
-        };
+		if (!register_action(enter_action)) {
+			msg("ifred action loading error");
+			return PLUGIN_SKIP;
+		};
 
-        update_action_shortcut("CommandPalette", "");
-    }
-    return r;
+		update_action_shortcut("CommandPalette", "");
+	}
+	return r;
 }
 
 //--------------------------------------------------------------------------
 void idaapi term(void) {
-    if (curWidget)
-        delete curWidget;
+	if (curWidget)
+		delete curWidget;
 }
 
 //--------------------------------------------------------------------------
@@ -201,21 +202,21 @@ void idaapi term(void) {
 //
 //--------------------------------------------------------------------------
 plugin_t PLUGIN =
-        {
-                IDP_INTERFACE_VERSION,
-                PLUGIN_FIX | PLUGIN_HIDE, // plugin flags
-                init,                     // initialize
+{
+		IDP_INTERFACE_VERSION,
+		PLUGIN_FIX | PLUGIN_HIDE, // plugin flags
+		init,                     // initialize
 
-                term, // terminate. this pointer may be NULL.
+		term, // terminate. this pointer may be NULL.
 
-                run, // invoke plugin
+		run, // invoke plugin
 
-                comment, // long comment about the plugin
-                // it could appear in the status line
-                // or as a hint
+		comment, // long comment about the plugin
+		// it could appear in the status line
+		// or as a hint
 
-                help, // multiline help about the plugin
+		help, // multiline help about the plugin
 
-                wanted_name, // the preferred short name of the plugin
-                ""           // wanted_hotkey; the preferred hotkey to run the plugin
-        };
+		wanted_name, // the preferred short name of the plugin
+		""           // wanted_hotkey; the preferred hotkey to run the plugin
+};
--- palette/src/utils.cpp
+++ palette/src/utils.cpp
@@ -4,30 +4,32 @@
 bool static_updated;
 
 const QString pluginPath(const char *filename) {
-    return (QDir::homePath() + "/take/" + filename);
+	static QString g_plugin_path;
+	if (g_plugin_path.size())
+		return g_plugin_path + filename;
+
+	g_plugin_path = (QDir::homePath() + "/take/theme/");
+	return g_plugin_path + filename;
 }
 
 QString loadFile(const char *filename, bool &updated) {
-    static QHash<QString, std::pair<long, QString>> last_loaded;
+    static QHash<QString, QString> last_loaded;
 
     auto absolutePath = pluginPath(filename);
     QFile file(absolutePath);
 
     updated = false;
 
-    if (!file.exists()) {
-        return QString();
-    }
-
-    long timestamp = QFileInfo(file).lastModified().toTime_t();
-
     if (last_loaded.contains(absolutePath)) {
-        auto &csspair = last_loaded[absolutePath];
-        if (csspair.first >= timestamp) {
-            return csspair.second;
-        }
+        auto &content = last_loaded[absolutePath];
+        return content;
     }
-    FILE *fp = qfopen(absolutePath.toStdString().c_str(), "rb");
+
+	if (!file.exists()) {
+		return QString();
+	}
+
+	FILE *fp = qfopen(absolutePath.toStdString().c_str(), "rb");
 
     if (fp == nullptr) {
         return QString();
@@ -42,8 +44,8 @@ QString loadFile(const char *filename, bool &updated) {
 
     buf[size] = '\0';
 
-    auto csspair = std::pair<long, QString>(timestamp, buf);
-    last_loaded[absolutePath] = csspair;
+    auto content = QString(buf);
+    last_loaded[absolutePath] = content;
 
     updated = true;
 
@@ -51,7 +53,7 @@ QString loadFile(const char *filename, bool &updated) {
 
     delete[] buf;
 
-    return csspair.second;
+    return content;
 }
 
 QJsonDocument cached_json;
